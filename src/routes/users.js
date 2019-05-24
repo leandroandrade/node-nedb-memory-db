1867fd1a24151d0b2fe86da1d1f50163972cd6f5
leandroandrade <leandro.andrade>
2019-05-24 10:55:37 -0300

    refactory

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,12 +2,12 @@
 
 const router = require('express').Router();
 
-const db = require('../database/db');
+module.exports = (db) => {
+    const saveController = require('../controllers/save-user-controller')(db);
+    const searchController = require('../controllers/search-all-user-controller')(db);
 
-const saveController = require('../controllers/save-user-controller')(db);
-const searchController = require('../controllers/search-all-user-controller')(db);
+    router.post('/', saveController.save)
+        .get('/', searchController.all);
 
-router.post('/', saveController.save)
-    .get('/', searchController.all);
-
-module.exports = router;
+    return router;
+};
